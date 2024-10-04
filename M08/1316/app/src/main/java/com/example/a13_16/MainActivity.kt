f8f2b18b6293bfa77638868c1c9042cb3c071473
Yago-Jodar <132337462+Yago-Jodar@users.noreply.github.com>
2024-10-04 17:29:27 +0200

    All layouts and images set

--- M08/1316/app/src/main/java/com/example/a13_16/MainActivity.kt
+++ M08/1316/app/src/main/java/com/example/a13_16/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.a13_16
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -11,5 +14,27 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
 
+        val games = arrayOf(
+            "Titanfall 2",
+            "Fire Emblem Three Houses",
+            "Call Of Duty MW2",
+            "Signalis",
+            "The Crew 2",
+            "Tom Clancy's Rainbow Six Siege",
+            "Shadow of the Tomb Raider"
+        )
+
+        val gamesList: ListView = findViewById<ListView>(R.id.gamesList)
+
+        val adapter = ArrayAdapter<String>(this, R.layout.games_list_item, games)
+
+        gamesList.adapter = adapter
+
+        gamesList.setOnItemClickListener()
+        { parent, view, position, id ->
+
+            val gameImg = findViewById<ImageView>(R.id.gamesListImg)
+            gameImg.setImageResource(R.drawable.    )
+        }
     }
 }
