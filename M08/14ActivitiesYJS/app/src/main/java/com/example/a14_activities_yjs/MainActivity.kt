64a11dfc0ea9442508af7634c920707bc1aa6fc1
Yago Jódar Segura <98105728+Yagueras@users.noreply.github.com>
2024-10-14 10:42:01 +0200

    Project1.4 activities uploaded

---
+++ M08/14ActivitiesYJS/app/src/main/java/com/example/a14_activities_yjs/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.a14_activities_yjs.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main) // Assuming you have an activity_main.xml layout file
+
+        
+    }
+}
