2095e83d6710f33b813763409d9536d467de829f
Yago-Jodar <132337462+Yago-Jodar@users.noreply.github.com>
2024-10-15 18:50:09 +0200

    Layout main set

--- M08/14ActivitiesYJS/app/src/main/java/com/example/a14_activities_yjs/MainActivity.kt
+++ M08/14ActivitiesYJS/app/src/main/java/com/example/a14_activities_yjs/MainActivity.kt
@@ -2,13 +2,20 @@ package com.example.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import com.example.a14_activities_yjs.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main) // Assuming you have an activity_main.xml layout file
+        setContentView(R.layout.activity_main)
 
-        
+
+        val userName = "cep"
+        val password = "informatica"
+
+        val inputName = findViewById<EditText>(R.id.userName)
+        val inputPassword = findViewById<EditText>(R.id.password)
+
     }
 }
