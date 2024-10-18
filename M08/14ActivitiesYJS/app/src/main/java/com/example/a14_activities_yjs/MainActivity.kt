9316ead1ef0829f90db97cd8970174f3ac5c7cf5
Yago-Jodar <132337462+Yago-Jodar@users.noreply.github.com>
2024-10-18 15:40:46 +0200

    Intent not quite working

--- M08/14ActivitiesYJS/app/src/main/java/com/example/a14_activities_yjs/MainActivity.kt
+++ M08/14ActivitiesYJS/app/src/main/java/com/example/a14_activities_yjs/MainActivity.kt
@@ -1,21 +1,48 @@
 package com.example.app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.a14_activities_yjs.ProfileCreator
 import com.example.a14_activities_yjs.R
 
 class MainActivity : AppCompatActivity() {
+
+    //En actividades que van a recoger un resultado, la actividad "padre", se queda esperando el resultado
+    private val getResult = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()){
+        if (it.resultCode == RESULT_OK){
+            val newUserName = it.data?.getStringExtra("newUser")
+            Toast.makeText(this, "Welcome $newUserName", Toast.LENGTH_SHORT).show()
+        }else if(it.resultCode == RESULT_CANCELED){
+            Toast.makeText(this, "Try again", Toast.LENGTH_SHORT).show()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val inputtedName = findViewById<EditText>(R.id.userName)
+        val inputtedPassword = findViewById<EditText>(R.id.password)
+        val loginButton = findViewById<Button>(R.id.loginButton)
 
-        val userName = "cep"
-        val password = "informatica"
-
-        val inputName = findViewById<EditText>(R.id.userName)
-        val inputPassword = findViewById<EditText>(R.id.password)
-
+        loginButton.setOnClickListener(){
+            if (inputtedName.text.isNotBlank()&&inputtedPassword.text.isNotBlank()) {
+                //Creamos el intent, y le damos tanto el contexto de la actividad actual, como la actividad que ha de lanzar
+                val intent = Intent(this, ProfileCreator::class.java)
+                //Después le damos los datos que querramos transferirle a esta nueva actividad
+                intent.putExtra("userName", inputtedName.text)
+                intent.putExtra("password", inputtedPassword.text)
+                startActivity(intent)
+            }else{
+                Toast.makeText(this, "You can't leave fields blank", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
---
+++ M08/14ActivitiesYJS/app/src/main/java/com/example/a14_activities_yjs/ProfileCreator.kt
@@ -0,0 +1,60 @@
+package com.example.a14_activities_yjs
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.app.MainActivity
+
+class ProfileCreator : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_profile_creator)
+
+        val userName = "cep"
+        val password = "informatica"
+
+        val intent = getIntent()
+        val givenUserName = intent.getStringExtra("userName")
+        val givenUserPassword = intent.getStringExtra("password")
+        var credentialsCheck = false
+
+        Toast.makeText(this, "$givenUserName", Toast.LENGTH_SHORT).show()
+        val newUserInput = findViewById<EditText>(R.id.nameInput)
+        val utilityButton = findViewById<Button>(R.id.createUserButton)
+
+        if (userName == givenUserName && password == givenUserPassword){
+            newUserInput.visibility = View.VISIBLE
+            utilityButton.setText("Create your user!")
+            credentialsCheck = true
+            Toast.makeText(this, "Credentials match", Toast.LENGTH_SHORT).show()
+        } else{
+            utilityButton.setText("Return")
+            Toast.makeText(this, "Credentials do not match", Toast.LENGTH_SHORT).show()
+        }
+
+        utilityButton.setOnClickListener(){
+            if (credentialsCheck){
+                if (newUserInput.text.isNotBlank()){
+                val result = Intent(this, MainActivity::class.java)
+                setResult(RESULT_OK, result)
+                result.putExtra("newUser", newUserInput.text)
+                finish()
+                }else{
+                    Toast.makeText(this, "Please input a valid name", Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                setResult(RESULT_CANCELED)
+                finish()
+            }
+        }
+    }
+}
