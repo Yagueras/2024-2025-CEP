1dc465aabb13e2041b4c51407d346265c4af761e
Yago-Jodar <132337462+Yago-Jodar@users.noreply.github.com>
2024-10-25 18:09:36 +0200

    logic for game entry creator on process

--- M08/1316/app/src/main/java/com/example/a13_16/GameEntryCreator.kt
+++ M08/1316/app/src/main/java/com/example/a13_16/GameEntryCreator.kt
@@ -1,6 +1,11 @@
 package com.example.a13_16
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class GameEntryCreator : AppCompatActivity(){
@@ -9,7 +14,21 @@ class GameEntryCreator : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.game_creator_activity)
 
+        val inputGameCover = findViewById<ImageButton>(R.id.newGameCover)
+        val inputGameName = findViewById<EditText>(R.id.newGameName)
+        val inputGamePublisher = findViewById<EditText>(R.id.newGamePublisher)
+        val inputGamePlayerCount = findViewById<EditText>(R.id.newGamePlayerCount)
+        val newEntryButton = findViewById<Button>(R.id.createEntryButton)
 
+        var selectedImage =
+
+        inputGameCover.setOnClickListener(){
+
+        }
+
+        newEntryButton.setOnClickListener(){
+            val newGameEntry = Game(inputGameName.text.toString(), selectedImage)
+        }
     }
 
     override fun onStop() {
