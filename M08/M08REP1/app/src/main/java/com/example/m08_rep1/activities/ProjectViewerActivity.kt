0bc2e7c2f2e908bceede3a8bb21e1fe4c94663c3
Yago-Jodar <132337462+Yago-Jodar@users.noreply.github.com>
2024-12-16 19:07:59 +0100

    can load ProjectViewerActivity on Activity start

--- M08/M08REP1/app/src/main/java/com/example/m08_rep1/activities/ProjectViewerActivity.kt
+++ M08/M08REP1/app/src/main/java/com/example/m08_rep1/activities/ProjectViewerActivity.kt
@@ -8,12 +8,19 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.m08_rep1.R
 import dataClasses.User
+import fragments.ProjectListFragment
 import tools.UserFileHandler
 
 class ProjectViewerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_project_view)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.project_view_fragment, ProjectListFragment())
+                .commit()
+        }
 
         val user = intent.getSerializableExtra("loggedUser") as User
         Toast.makeText(this, "Welcome back ${user.name}", Toast.LENGTH_SHORT).show()
