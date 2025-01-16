3957efddc6e0f3029745578618183660b51dbdc9
Yago-Jodar <132337462+Yago-Jodar@users.noreply.github.com>
2025-01-16 11:36:16 +0100

    Made the description value in SubTask.kt optional

--- M08/M08REP1/app/src/main/java/dataClasses/SubTask.kt
+++ M08/M08REP1/app/src/main/java/dataClasses/SubTask.kt
@@ -5,8 +5,9 @@ import java.io.Serializable
 data class SubTask(
     val subTaskID: Int,
     val name: String,
-    val description: String,
+    val description: String?,
     val requiredTime: Int,
     val timeUsed: Int,
+    val dueDate: String,
     val status: Int
 ) : Serializable
