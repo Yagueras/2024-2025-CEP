c4833cf5914e960efd8c17417b8b927730c16c4a
Yago-Jodar <132337462+Yago-Jodar@users.noreply.github.com>
2025-01-16 11:34:42 +0100

    Modified the status select button from fragment_task_menu.xml, to a spinner and wrote the logic to populate it

--- M08/M08REP1/app/src/main/java/fragments/TaskMenuFragment.kt
+++ M08/M08REP1/app/src/main/java/fragments/TaskMenuFragment.kt
@@ -7,13 +7,19 @@ import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.LinearLayout
+import android.widget.Spinner
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.m08_rep1.R
+import dataClasses.Status
 import dataClasses.Task
 
-class TaskMenuFragment(selectedTask: Task) : Fragment() {
+class TaskMenuFragment(val selectedTask: Task, val statusList: Array<Status>) : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,6 +32,38 @@ class TaskMenuFragment(selectedTask: Task) : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val currentStatus = view.findViewById<Spinner>(R.id.project_status_picker)
+        val timeUsedDisplay = view.findViewById<TextView>(R.id.time_used_display)
+        val timeRequiredDisplay = view.findViewById<TextView>(R.id.time_required_display)
+
+        val currentStatusAdapter:ArrayAdapter<String> = ArrayAdapter(requireContext(), androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, getStatusNames(statusList))
+        currentStatus.adapter = currentStatusAdapter
+
+        currentStatus.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                Toast.makeText(requireContext(), statusList[selectedTask.status].statusName, Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        currentStatus.setSelection(statusList[selectedTask.status].statusID)
+        timeUsedDisplay.text = selectedTask.timeUsed.toString()
+        timeRequiredDisplay.text = selectedTask.requiredTime.toString()
+    }
+
+    private fun getStatusNames(statusList: Array<Status>): ArrayList<String> {
+        val statusNameList: ArrayList<String> = arrayListOf()
+        var statusName: String
+
+        for (status in statusList){
+            statusName = status.statusName
+            statusNameList.add(statusName)
+        }
+        return statusNameList
     }
 
 }
